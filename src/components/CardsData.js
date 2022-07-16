8916e7ba5a539ecaa31749a37c020f14d6d7d19c
Divyam <divyamsharma822@gmail.com>
2022-07-16 23:48:18 +0530

    Initial commit

---
+++ src/components/CardsData.js
@@ -0,0 +1,76 @@
+const Cardsdata = [
+    {
+        id: 1,
+        rname: "LV White tshirt",
+        imgdata:
+            "https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80",
+        delimg: "https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80",
+        somedata: " 1175 + order placed from here recently",
+        price: 350,
+        rating: "3.8",
+        arrimg: "https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80",
+        qnty: 0,
+    },
+    {
+        id: 2,
+        rname: "Gucci Blue Tshirt",
+        imgdata:
+            "https://images.unsplash.com/photo-1621951753015-740c699ab970?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        delimg: "https://images.unsplash.com/photo-1621951753015-740c699ab970?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        somedata: " 2525 + order placed from here recently",
+        price: 250,
+        rating: "3.9",
+        arrimg: "https://images.unsplash.com/photo-1621951753015-740c699ab970?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        qnty: 0,
+    },
+    {
+        id: 3,
+        rname: "Checked Tshirt",
+        imgdata:
+            "https://images.unsplash.com/photo-1630167146816-e1f4ff99c00c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        delimg: "https://images.unsplash.com/photo-1630167146816-e1f4ff99c00c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        somedata: " 650 + order placed from here recently",
+        price: 700,
+        rating: "4.2",
+        arrimg: "https://images.unsplash.com/photo-1630167146816-e1f4ff99c00c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        qnty: 0,
+    },
+    {
+        id: 4,
+        rname: "Laundry Sweatshirt",
+        imgdata: 
+            "https://images.unsplash.com/photo-1600609842388-3e2ed2a1454a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        delimg: "https://images.unsplash.com/photo-1593031023487-27d443f880e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        somedata: " 300 + order placed from here recently",
+        price: 700,
+        rating: "3.8",
+        arrimg: "https://images.unsplash.com/photo-1593031023487-27d443f880e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        qnty: 0,
+    },
+    {
+        id: 5,
+        rname: "Blue Denim Jeans",
+        imgdata:
+            "https://images.unsplash.com/photo-1574809736435-dced6ecf6ce7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        delimg: "https://images.unsplash.com/photo-1574809736435-dced6ecf6ce7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        somedata: "1050 + order placed from here recently",
+        price: 210,
+        rating: "4.0",
+        arrimg: "https://images.unsplash.com/photo-1574809736435-dced6ecf6ce7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        qnty: 0,
+    },
+    {
+        id: 6,
+        rname: "Jacob Perl Glasses",
+        imgdata:
+            "https://images.unsplash.com/photo-1511499767150-a48a237f0083?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        delimg: "https://images.unsplash.com/photo-1511499767150-a48a237f0083?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        somedata: " 1100 + order placed from here recently",
+        price: 100,
+        rating: "3.8",
+        arrimg: "https://images.unsplash.com/photo-1511499767150-a48a237f0083?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        qnty: 0,
+    }
+];
+
+export default Cardsdata;
